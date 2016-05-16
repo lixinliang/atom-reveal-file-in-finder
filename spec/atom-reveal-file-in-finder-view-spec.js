e1bba531d13dcacce51cd4db58fd7701881c19a0
李昕亮 <lixinliang@yy.com>
2016-05-16 14:38:28 +0800

    update

--- spec/atom-reveal-file-in-finder-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomRevealFileInFinderView from '../lib/atom-reveal-file-in-finder-view';
-
-describe('AtomRevealFileInFinderView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
